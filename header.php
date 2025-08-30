3569f1f5a21f9486d16451afea2bd2767d77e212
rmkdunn <34885209+rmkdunn@users.noreply.github.com>
2025-08-30 15:17:41 -0500

    Add files via upload

--- header.php
+++ header.php
@@ -87,14 +87,16 @@
                 <li class="nav-item">
                     <a class="nav-link" href="logout.php">Logout</a>
                 </li>
-                </div>
+          
             </ul>
             <form class="form-inline my-2 my-lg-0" method="GET" action="search.php">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
+            
             <div class="theme-switcher text-white">
                 <span id="theme-icon">🌙</span> Dark Mode
+            </div>
             </div>
         </div>
     </nav>
